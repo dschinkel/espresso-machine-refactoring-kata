5d2947b617d6119af6a217325e273d8cc49daef2
dschinkel <dschinkel@gmail.com>
2018-08-08 14:59:05 -0500

    brainstorming on making a messy espresso machine api

---
+++ EspressoMachine.js
@@ -0,0 +1,44 @@
+/*
+ pull()(note: pullShot() would be a better name)
+    - this sets the courseness level
+    - this kicks off the brewing.  The brewing is another nested if statement inside this pull shot
+
+ dispenseSteam()
+
+ dispenseWater()
+    
+  Also this is just a list of functions inside a file.  There really isn't a true module here.  It's just a bunch of functions
+  grouped in a file that has no underlying structure to it really
+*/
+
+function run(turnOn, turnOff, pull, steam, water) {
+  /*
+    default shot - standard
+    if(turnOn)
+      ...
+
+    if(turnOff)
+      ...
+
+    if(pull)
+      // preinfusion
+        - default length/time
+            me: this is really a setting that needs to be tweak-able to allow different shot types
+
+      // set up shot
+        - default hard coded shot temp
+        - default hard coded shot pull time
+
+    if(steam)
+      // run steam
+        - default hard coded temp
+        - default hard coded pressure
+
+    if(water)
+      // run water
+        - default temp
+  */
+}
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# espresso-machine-refactoring-kata
-Practice writing Tests &amp; SOLID Principles by refactoring the messy virtual espresso machine
+espresso machine refactoring kata
+=================================
+
+We have a virtual espresso machine expressed in one big messy module called EspressMachine.
+
+#### Your job is to perform the following in this order
+**write tests around the messy legacy code**
+ - write integration tests around the EspressoMachine module so that you can start to refactor with at least some degree of safety
+
+**refactor Espressomachine** using [SOLID Principles](http://butunclebob.com/ArticleS.UncleBob.PrinciplesOfOod)
+- add new features to the virtual espresso machine
